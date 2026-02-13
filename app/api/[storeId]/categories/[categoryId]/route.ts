fed6b11de80bebf80822147c29724f819a03c7c0
Faiz-Super <faizmhmad15@gmail.com>
2026-02-13 10:56:38 +0700

    Kategori admin section

---
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -0,0 +1,125 @@
+import db from "@/lib/db";
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+
+export async function GET (
+    req: Request,
+    props: {params: Promise<{categoryId: string}>} 
+) {
+    try {
+
+        const params = await props.params;
+
+        if(!params.categoryId){
+            return new NextResponse("Category Id dibutuhkan", {status: 400});
+        }
+        
+        const category = await db.category.findUnique({
+            where: {
+                id: params.categoryId,
+            },
+        });
+        return NextResponse.json(category);    
+        
+    } catch (error) {
+        console.log('[CATEGORIES_GET]', error);
+        return new NextResponse("Internal Error", {status: 500})
+    }
+}
+
+export async function PATCH (
+    req: Request,
+    props: {params: Promise<{storeId: string, categoryId: string}>}
+) {
+    try {
+
+        const {userId} = await auth();
+        const body = await req.json();
+
+        const {name, bannerId} = body;
+
+        const params = await props.params;
+
+        if(!userId){
+            return new NextResponse("Unauthenticated", {status: 401});
+        }
+        if(!name){
+            return new NextResponse("Harus menginput nama" , {status: 400});
+        }
+        if(!bannerId){
+            return new NextResponse("Harus menginput bannerId" , {status: 400});
+        }
+        if(!params.categoryId){
+            return new NextResponse("Category Id dibutuhkan", {status: 400});
+        }
+
+        const storeByUserId = await db.store.findFirst ({
+            where: {
+                id: params.storeId,
+                userId
+            }
+        })
+
+        if(!storeByUserId){
+            return new NextResponse("Unauthorized", {status: 403})
+        }
+        
+        const category = await db.category.updateMany({
+            where: {
+                id: params.categoryId,
+            },
+            data: {
+                name,
+                bannerId,
+            },
+        });
+        return NextResponse.json(category);    
+        
+    } catch (error) {
+        console.log('[CATEGORY_PATCH]', error)
+        return new NextResponse("Internal Error", {status: 500})
+    }
+}
+
+export async function DELETE (
+    req: Request,
+    props: {params: Promise<{storeId: string, categoryId: string}>} 
+) {
+    try {
+
+        const {userId} = await auth()
+        const params = await props.params;
+       
+
+        if(!userId){
+            return new NextResponse("Unauthenticated", {status: 401});
+        }
+        
+        if(!params.categoryId){
+            return new NextResponse("Category Id dibutuhkan", {status: 400});
+        }
+
+        const storeByUserId = await db.store.findFirst ({
+            where: {
+                id: params.storeId,
+                userId
+            }
+        })
+
+        if(!storeByUserId){
+            return new NextResponse("Unauthorized", {status: 403})
+        }
+        
+        const category = await db.category.deleteMany({
+            where: {
+                id: params.categoryId,
+            },
+        });
+        return NextResponse.json(category);    
+        
+    } catch (error) {
+        console.log('[CATEGORY_DELETE]', error)
+        return new NextResponse("Internal Error", {status: 500})
+    }
+}
